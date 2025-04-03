ed732f701d589aa0adef8f68369740d8b93a5a5a
Mig Fox <mtg20178@gmail.com>
2025-04-03 19:09:44 -0300

    Removendo teste.js do rastreamento do Git

--- teste.js
+++
@@ -1,9 +0,0 @@
-const input = require("synchro-prompt");
-
-let a = input("Digite o primeiro valor ");
-let b = input("Digite o segundo valor");
-let c = input("Digite o terceiro valor");
-
-let vet = [a,b,c];
-
-console.log(`1: ${vet[0]}\n2: ${vet[1]}\n3: ${vet[2]}`);
